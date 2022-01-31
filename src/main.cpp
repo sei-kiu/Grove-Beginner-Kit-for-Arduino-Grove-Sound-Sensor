b23d1210e9e93545d0fdbcb013d526a8e963c9df
sei-kiu <sei.kiu.sei.kiu@gmail.com>
2022-01-31 08:55:38 +0800

    working code

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+//Sound Control Light
+int soundPin = A6; // Analog sound sensor is to be attached to analog
+int ledPin = 4; // Digital LED is to be attached to digital
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(soundPin, INPUT);
+  Serial.begin(9600);
+}
+void loop(){
+  int soundState = analogRead(soundPin); // Read sound sensor’s value
+  Serial.println(soundState);
+  // if the sound sensor’s value is greater than 400, the light will be on.
+  //Otherwise, the light will be turned off
+  if (soundState > 400) {
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+  }else{
+    digitalWrite(ledPin, LOW);
+  }
+}
